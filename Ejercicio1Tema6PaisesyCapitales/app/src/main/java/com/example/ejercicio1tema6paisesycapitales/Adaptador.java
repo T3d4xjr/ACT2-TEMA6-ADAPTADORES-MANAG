8530636f4a511fb8fddcde0f7abbe1162b0b6e9e
T3d4xjr <tedaxjr@gmail.com>
2024-11-07 18:18:17 +0100

    UPD:Ejercicio 1

--- Ejercicio1Tema6PaisesyCapitales/app/src/main/java/com/example/ejercicio1tema6paisesycapitales/Adaptador.java
+++ Ejercicio1Tema6PaisesyCapitales/app/src/main/java/com/example/ejercicio1tema6paisesycapitales/Adaptador.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class Adaptador extends ArrayAdapter<Datos> {
@@ -20,13 +21,50 @@ public class Adaptador extends ArrayAdapter<Datos> {
         LayoutInflater inflater = LayoutInflater.from(getContext());
         View elemento = inflater.inflate(R.layout.elemento, parent, false);
 
-        // Obtener referencias a los TextView del layout elemento.xml
+        // Obtener referencias a los TextView y ImageView del layout
         TextView texto1 = elemento.findViewById(R.id.texto1);
         TextView texto2 = elemento.findViewById(R.id.texto2);
+        ImageView imagenBandera = elemento.findViewById(R.id.imagenBandera);
 
         // Asignar los valores de los datos actuales a los TextView
-        texto1.setText(datos[position].getTexto1());
-        texto2.setText(datos[position].getTexto2());
+        texto1.setText(datos[position].getTexto1());  // Nombre del país
+        texto2.setText(datos[position].getTexto2());  // Capital
+
+        // Asignar la imagen de la bandera correspondiente según el país
+        switch (datos[position].getTexto1()) {
+            case "España":
+                imagenBandera.setImageResource(R.drawable.espana);
+                break;
+            case "Francia":
+                imagenBandera.setImageResource(R.drawable.francia);
+                break;
+            case "Venezuela":
+                imagenBandera.setImageResource(R.drawable.venezuela);
+                break;
+            case "Alemania":
+                imagenBandera.setImageResource(R.drawable.alemania);
+                break;
+            case "Irlanda":
+                imagenBandera.setImageResource(R.drawable.irlanda);
+                break;
+            case "Noruega":
+                imagenBandera.setImageResource(R.drawable.noruega);
+                break;
+            case "Italia":
+                imagenBandera.setImageResource(R.drawable.italia);
+                break;
+            case "Argentina":
+                imagenBandera.setImageResource(R.drawable.argentina);
+                break;
+            case "Canadá":
+                imagenBandera.setImageResource(R.drawable.canada);
+                break;
+            case "Portugal":
+                imagenBandera.setImageResource(R.drawable.portugal);
+                break;
+            default:
+                 // Bandera por defecto si no hay coincidencia
+        }
 
         return elemento;
     }
